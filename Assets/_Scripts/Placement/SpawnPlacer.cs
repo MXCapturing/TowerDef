4af214c1793b243b1be017d71c714862175c0ef0
Unknown <MXCapturing@gmail.com>
2019-05-03 21:59:47 +1200

    Rocketlauncher added

--- Assets/_Scripts/Placement/SpawnPlacer.cs
+++ Assets/_Scripts/Placement/SpawnPlacer.cs
@@ -6,6 +6,7 @@ public class SpawnPlacer : MonoBehaviour {
 
     public GameObject machineGun;
     public GameObject flamethrower;
+    public GameObject rocketlauncher;
 
     public void SpawnMachineGun()
     {
@@ -17,5 +18,11 @@ public class SpawnPlacer : MonoBehaviour {
     {
         ShopMenu.instance.shopMenu.SetActive(false);
         Instantiate(flamethrower, Vector3.zero, Quaternion.identity);
+    }
+
+    public void SpawnRocketLauncher()
+    {
+        ShopMenu.instance.shopMenu.SetActive(false);
+        Instantiate(rocketlauncher, Vector3.zero, Quaternion.identity);
     }
 }
