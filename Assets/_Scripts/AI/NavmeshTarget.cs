2f1c5ad0f5cc6855ef21a58b249dc0fe43c32c17
Connor Isogai <MXCapturing@gmail.com>
2019-05-13 15:33:45 +1200

    Hopeful fix

--- Assets/_Scripts/AI/NavmeshTarget.cs
+++ Assets/_Scripts/AI/NavmeshTarget.cs
@@ -15,6 +15,8 @@ public class NavmeshTarget : MonoBehaviour {
 
     public Animator _anim;
 
+    public bool attacking;
+    public bool dead;
     public bool trapped;
     public float maxSpeed;
 
@@ -26,14 +28,17 @@ public class NavmeshTarget : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(phases.gamePhases == Phases.FPS && trapped == false)
+        if(phases.gamePhases == Phases.FPS && trapped == false && dead == false && attacking == false)
         {
             agent.speed = maxSpeed;
+            agent.isStopped = false;
             _anim.SetBool("Walking", true);
         }
-        if(phases.gamePhases == Phases.Build || trapped == true)
+        if(phases.gamePhases == Phases.Build || trapped == true || dead == true || attacking == true)
         {
             agent.speed = 0;
+            agent.velocity = Vector3.zero;
+            agent.isStopped = true;
             _anim.SetBool("Walking", false);
         }
 
@@ -46,4 +51,17 @@ public class NavmeshTarget : MonoBehaviour {
             targetFinder.SetActive(false);
         }
 	}
+
+    private void FixedUpdate()
+    {
+        if(GetComponent<Rigidbody>().isKinematic == true)
+        {
+           // Invoke("KinematicOff", 0.5f);
+        }
+    }
+
+    void KinematicOff()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
 }
